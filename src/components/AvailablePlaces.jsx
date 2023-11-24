1fc2ea7011850ce1bf2647b4c2f10046817dbbe6
SameeraDayaratne <85645373+SameeraDayaratne@users.noreply.github.com>
2023-11-24 14:39:55 +0530

    Implemented the get request for places

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,14 +1,27 @@
-import Places from './Places.jsx';
-import { useState } from 'react';
+import Places from "./Places.jsx";
+import { useState, useEffect } from "react";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  const [AvailablePlaces ,setAvailablePlaces] = useState();
+  useEffect(() => {
+
+    async function fetchPlaces(){
+
+      let response = await fetch("http://localhost:3000/places");
+      let resData = await response.json();
+      setAvailablePlaces(resData.places);
+
+    }
+
+    fetchPlaces();
+    
+  }, []);
 
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
